d6462e1d3cd4f9ba13563d290e01a4044da82618
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-01-20 00:54:56 -0300

    ♻️ refactor: remove carousel and parallax

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,21 @@
 <template>
 	<div>
-		<v-parallax src="/office2.jpg">
-			<v-spacer> </v-spacer>
-			<v-row justify="center" align="center">
-				<v-col cols="12" sm="8" md="6">
-					<div class="text-center"></div>
-					<v-card elevation="10">
-						<v-card-title class="headline">
-							Ricardo Calil & Advogados Associados
-						</v-card-title>
-						<v-card-text>
-							<p>Seja bem-vindo ao nosso escritório!</p>
-						</v-card-text>
-						<v-card-actions> </v-card-actions>
-					</v-card>
-				</v-col>
-			</v-row>
-			<v-spacer> </v-spacer>
-		</v-parallax>
-
+		<v-row justify="center" align="center">
+			<v-col cols="12" sm="8" md="6">
+				<div class="text-center"></div>
+				<v-card elevation="10">
+					<v-card-title class="headline">
+						Ricardo Calil & Advogados Associados
+					</v-card-title>
+					<v-card-text>
+						<p>Seja bem-vindo ao nosso escritório!</p>
+					</v-card-text>
+					<v-card-actions> </v-card-actions>
+				</v-card>
+			</v-col>
+		</v-row>
 		<lawyer-cards> </lawyer-cards>
 	</div>
-	<!--v-carousel cycle hide-delimiter-background show-arrows-on-hover>
-		<v-carousel-item
-			v-for="(slide, i) in slides"
-			:key="i"
-			:src="slide.src"
-			contain
-		></v-carousel-item>
-	</v-carousel-->
 </template>
 
 <script>
@@ -39,22 +26,7 @@
 		},
 		layout: (ctx) => (ctx.isMobile ? 'mobile' : 'default'),
 		data() {
-			return {
-				slides: [
-					{
-						src: '/law.jpg',
-					},
-					{
-						src: '/law2.jpg',
-					},
-					{
-						src: '/book.jpg',
-					},
-					{
-						src: '/books.jpg',
-					},
-				],
-			}
+			return {}
 		},
 	}
 </script>
